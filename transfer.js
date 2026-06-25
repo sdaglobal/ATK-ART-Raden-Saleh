f1ab318ad61f21f19b2e7d88992d717a13533c58
sdaglobal <sdaglobal988@gmail.com>
2026-06-25 16:05:05 +0700

    Update transfer.js

--- transfer.js
+++ transfer.js
@@ -1,13 +1,87 @@
-const supabaseUrl =
-"https://zrftcvy...supabase.co";
+/* ===========================================================
+   ATK ART TRANSFER BARANG
+   Version : 1.0
+=========================================================== */
 
-const supabaseKey =
-"sb_publishable_xxxxx";
 
-const db =
+/* ===========================================================
+   SUPABASE CONFIG
+=========================================================== */
+
+// ==========================
+// RADEN SALEH
+// ==========================
+
+const RADEN_URL =
+"https://wlnibcoqspxwmylkyolk.supabase.co";
+
+const RADEN_KEY =
+"ISI_PUBLISHABLE_KEY_RADEN";
+
+/* ==========================
+   MARGOMULYO
+========================== */
+
+const MARGO_URL =
+"https://jihscriziqgeelgguhuf.supabase.co";
+
+const MARGO_KEY =
+"ISI_PUBLISHABLE_KEY_MARGO";
+
+/* ==========================
+   TRANSFER CENTER
+========================== */
+
+const TRANSFER_URL =
+"https://zrftcvymxhqoevgmvmep.supabase.co";
+
+const TRANSFER_KEY =
+"ISI_PUBLISHABLE_KEY_TRANSFER";
+
+
+/* ===========================================================
+   DATABASE CONNECTION
+=========================================================== */
+
+const dbRaden =
 supabase.createClient(
-supabaseUrl,
-supabaseKey
+RADEN_URL,
+RADEN_KEY
 );
 
-console.log("Transfer Ready");
+const dbMargo =
+supabase.createClient(
+MARGO_URL,
+MARGO_KEY
+);
+
+const dbTransfer =
+supabase.createClient(
+TRANSFER_URL,
+TRANSFER_KEY);
+
+
+/* ===========================================================
+   GLOBAL VARIABLE
+=========================================================== */
+
+let SITE = "Raden Saleh";
+
+let daftarBarang = [];
+
+let daftarTransfer = [];
+
+let detailTransfer = [];
+
+let currentTransfer = null;
+
+
+/* ===========================================================
+   INIT
+=========================================================== */
+
+document.addEventListener("DOMContentLoaded", async ()=>{
+
+    console.log("Transfer Ready");
+
+});
